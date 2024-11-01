a21197af20a0dfd7f1d1f68500a877ce7a5748b7
Hyewon Yoon <iey704@gachon.ac.kr>
2024-11-01 18:13:51 +0900

    chore: update config files

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,31 @@
 module.exports = {
   root: true,
-  extends: '@react-native',
+  extends: ['airbnb', 'airbnb/hooks', 'prettier'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+    project: './tsconfig.json',
+  },
+  plugins: ['@typescript-eslint', 'prettier'],
+  rules: {
+    'prettier/prettier': ['error'],
+    'no-console': 'off',
+    'react-native/no-inline-styles': 'off',
+    'import/extensions': 'off',
+    'import/no-unresolved': 'off',
+    'react/jsx-filename-extension': [1, {extensions: ['.tsx', '.jsx']}],
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-unused-vars': 'warn',
+  },
+  settings: {
+    'import/parsers': {
+      '@typescript-eslint/parser': ['.ts', '.tsx'],
+    },
+    'import/resolver': {
+      typescript: {
+        alwaysTryTypes: true, // @types/* 모듈도 확인합니다
+      },
+    },
+  },
 };
