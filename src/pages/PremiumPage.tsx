733af3b361f75a632c06b938008c72731784030d
Hyewon Yoon <iey704@gachon.ac.kr>
2024-11-01 18:15:23 +0900

    feat: implement each pages

---
+++ src/pages/PremiumPage.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+export default function PremiumPage() {
+  return (
+    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+      <Text>프리미엄</Text>
+    </View>
+  );
+}
---
+++ src/pages/TicketBookPage.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+export default function TicketBookPage() {
+  return (
+    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+      <Text>티켓북</Text>
+    </View>
+  );
+}
