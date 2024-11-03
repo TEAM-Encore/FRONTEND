0ffb9d6749a32076abb541445e9bee7ae02154bd
nnyouung <211_0@kookmin.ac.kr>
2024-11-03 09:00:36 +0900

    feat: 홈 아이콘 및 이미지 추가

---
+++ src/components/home/IconDate.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Svg, {G, Path, SvgProps} from 'react-native-svg';
+
+const IconDate: React.FC<SvgProps> = props => (
+  <Svg width="11" height="12" viewBox="0 0 11 12" fill="none">
+    <G id="Frame">
+      <Path
+        id="Vector"
+        d="M5.50002 10.5834C2.96871 10.5834 0.916687 8.53136 0.916687 6.00008C0.916687 3.46878 2.96871 1.41675 5.50002 1.41675C8.0313 1.41675 10.0834 3.46878 10.0834 6.00008C10.0834 8.53136 8.0313 10.5834 5.50002 10.5834ZM5.95835 6.00008V3.70841H5.04169V6.91675H7.79169V6.00008H5.95835Z"
+        fill="#FBFBFB"
+      />
+    </G>
+  </Svg>
+);
+
+export default IconDate;
