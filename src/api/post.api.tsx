ae555bf7993e3e8a29b845c5ef935ba0be40c4d4
Hyewon Yoon <iey704@gachon.ac.kr>
2024-11-15 23:59:49 +0900

    refactor: 게시글 조회 api 수정

--- src/api/post.api.tsx
+++ src/api/post.api.tsx
@@ -42,18 +42,22 @@ export const DeletePost = (post_id: number) => {
 
 // 게시글 페이징 조회
 export const GetPostList = (
-  pageable: object,
+  page: number,
+  size: number,
+  sort: string,
   cursor?: number,
   category?: string,
   type?: string,
   search_word?: string,
 ) => {
   const requestParams = {
+    page,
+    size,
+    sort,
     cursor,
     category,
     type,
     search_word,
-    pageable: JSON.stringify(pageable),
   };
   return httpApi.get(`/api/v1/post/list`, {
     params: requestParams,
--- src/pages/dashboard/free/FreeList.tsx
+++ src/pages/dashboard/free/FreeList.tsx
@@ -19,12 +19,19 @@ const FreeList: React.FC<FreeListProps> = ({selectedFilter}) => {
   useEffect(() => {
     const fetchPostList = async () => {
       const sortFilter = filterMapping[selectedFilter] || 'createdat';
-      const pageable = {size: 3, sort: {sortFilter}};
       console.log('선택된 필터: ', selectedFilter);
 
       try {
         setLoading(true);
-        const response = await GetPostList(pageable, undefined, 'FREE');
+        const response = await GetPostList(
+          0,
+          100,
+          sortFilter,
+          undefined,
+          undefined,
+          'FREE',
+          undefined,
+        );
         console.log('API RESPONSE:', response.data);
 
         const postData = response.data.data.content;
