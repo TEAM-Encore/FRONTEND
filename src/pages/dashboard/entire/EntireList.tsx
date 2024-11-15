68baaa75ea30f42680764862dd451b23e7e1a517
Hyewon Yoon <iey704@gachon.ac.kr>
2024-11-15 16:09:52 +0900

    feat: 전체 게시글 조회 api 연결

---
+++ src/pages/dashboard/entire/EntireList.tsx
@@ -0,0 +1,51 @@
+import React, {useState, useEffect} from 'react';
+import {View, Text} from 'react-native';
+import ItemPost from '@/components/comment/ItemPost';
+import {GetPostList} from '../../../api/post.api'; // @ 경로 사용하면 오류 발생하는데 원인을 모르겠음
+
+type EntireListProps = {
+  selectedFilter: string;
+};
+
+const EntireList: React.FC<EntireListProps> = ({selectedFilter}) => {
+  const [postList, setPostList] = useState<any[]>([]);
+  const [loading, setLoading] = useState<boolean>(true);
+
+  const filterMapping: Record<string, string> = {
+    최신순: 'createdat',
+    인기순: 'likecount',
+  };
+
+  useEffect(() => {
+    const fetchPostList = async () => {
+      const sortFilter = filterMapping[selectedFilter] || 'createdat';
+      const pageable = {size: 3, sort: {sortFilter}};
+      console.log('선택된 필터: ', selectedFilter);
+      try {
+        setLoading(true);
+        const response = await GetPostList(pageable);
+        console.log('API RESPONSE:', response.data);
+
+        const postData = response.data.data.content;
+        setPostList(postData);
+        console.log('postList:', postData);
+      } catch (error) {
+        console.error('Error fetching post list:', error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchPostList();
+  }, [selectedFilter]);
+
+  return (
+    <>
+      <View>
+        {/* 추후 로딩 페이지 추가 필요 */}
+        {loading ? <Text>Loading...</Text> : <ItemPost postList={postList} />}
+      </View>
+    </>
+  );
+};
+export default EntireList;
