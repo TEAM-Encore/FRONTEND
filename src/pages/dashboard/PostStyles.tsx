0ad6eff1ca36b949795e2fdc4ff4c64aa7919a80
Hyewon Yoon <iey704@gachon.ac.kr>
2024-11-15 17:24:47 +0900

    chore: 스타일 관련 파일 수정

---
+++ src/pages/dashboard/PostStyles.tsx
@@ -0,0 +1,43 @@
+import {StyleSheet} from 'react-native';
+import Colors from '@/assets/colors/Colors';
+import {typography} from '../../styles/typography';
+
+const {subhead02, subhead03, body01, caption} = typography;
+
+// 정보, 후기, 배우, 자유 게시판에서 사용되는 스타일
+const PostStyles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: Colors.gray_01,
+  },
+  containerRow: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  containerCommentTitle: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    marginTop: 25,
+    marginBottom: 18,
+    marginLeft: 24,
+    marginRight: 20,
+  },
+  textCategory: {
+    ...subhead03,
+    marginRight: 7,
+  },
+  textLatestRecommended: {
+    ...body01,
+    color: '#8B8B8B',
+  },
+  filter: {
+    ...subhead02,
+  },
+  tab_filter: {
+    ...body01,
+    color: Colors.gray_07,
+  },
+});
+
+export default PostStyles;
