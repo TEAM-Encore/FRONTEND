cd80336dc2187a22aa98d7e312662ad10da68d6e
Hyewon Yoon <iey704@gachon.ac.kr>
2024-11-15 17:26:26 +0900

    feat: 배우 게시판 api 연결

--- src/pages/dashboard/actor/ActorScreen.tsx
+++ src/pages/dashboard/actor/ActorScreen.tsx
@@ -1,11 +1,63 @@
-import React from 'react';
-import {View, Text} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  View,
+  Text,
+  TouchableOpacity,
+} from 'react-native';
 
-const ActorScreen: React.FC = () => {
+import PostStyles from '../PostStyles';
+import ActorList from './ActorList';
+
+type ActorScreenProps = {};
+
+const ActorScreen: React.FC<ActorScreenProps> = () => {
+  const [modalVisible, setModalVisible] = useState(false);
+  const [selectedFilter, setSelectedFilter] = useState<'최신순' | '인기순'>(
+    '최신순',
+  );
+
+  const handlePress = (filter: '최신순' | '인기순') => {
+    if (selectedFilter !== filter) {
+      setSelectedFilter(filter);
+    }
+  };
+
   return (
-    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-      <Text>ActorScreen!</Text>
-    </View>
+    <SafeAreaView style={PostStyles.container}>
+      <ScrollView>
+        <View style={PostStyles.containerCommentTitle}>
+          <Text style={PostStyles.textCategory}>전체글</Text>
+
+          <View style={PostStyles.containerRow}>
+            <TouchableOpacity onPress={() => handlePress('최신순')}>
+              <Text
+                style={[
+                  selectedFilter === '최신순'
+                    ? PostStyles.filter
+                    : PostStyles.tab_filter,
+                  {marginRight: 12},
+                ]}>
+                최신순
+              </Text>
+            </TouchableOpacity>
+            <TouchableOpacity onPress={() => handlePress('인기순')}>
+              <Text
+                style={[
+                  selectedFilter === '최신순'
+                    ? PostStyles.tab_filter
+                    : PostStyles.filter,
+                ]}>
+                인기순
+              </Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+
+        <ActorList selectedFilter={selectedFilter} />
+      </ScrollView>
+    </SafeAreaView>
   );
 };
 
