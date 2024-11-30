5a75c9837ef6be6d2da3a58701c26f730ebbb644
Hyewon Yoon <iey704@gachon.ac.kr>
2024-11-30 17:55:21 +0900

    feat: 임시저장 목록 로딩 화면 추가 및 데이터가 없을 때 예외 처리

--- src/pages/write/save/SavePage.tsx
+++ src/pages/write/save/SavePage.tsx
@@ -6,50 +6,64 @@ import {
   Text,
   TouchableOpacity,
   Alert,
+  ActivityIndicator,
 } from 'react-native';
 import SaveStyles from './SaveStyles';
-import AlertModal from '@/components/alertModal/AlertModal';
+import DeleteTempModal from '@/components/alertModal/DeleteTempModal';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import {getPost} from '@/api/post.api';
 import {timeAgo} from '@/util/timeAgo';
 
 const SavePage: React.FC = () => {
+  const [isLoading, setIsLoading] = useState(true);
   const [modalVisible, setModalVisible] = useState(false);
   const [selectedTitle, setSelectedTitle] = useState('');
   const [selectedSubTitle, setSelectedSubtitle] = useState('');
   const [topButton, setTopButton] = useState('');
   const [bottomButton, setBottomButton] = useState('');
   const [selectedPostId, setSelectedPostId] = useState<number | null>(null);
-
   const [count, setCount] = useState(0);
   const [savedPosts, setSavedPosts] = useState<any[]>([]);
 
-  useEffect(() => {
-    const fetchSavedPosts = async () => {
-      try {
-        // AsyncStorage에 저장되어 있는 데이터 확인
-        const storedData = await AsyncStorage.getItem('temporaryPosts');
-        const parsedData = JSON.parse(storedData || '[]');
+  const fetchSavedPosts = async () => {
+    try {
+      const storedData = await AsyncStorage.getItem('temporaryPosts');
+      console.log('스토리지에 저장된 임시 저장 글: ', storedData);
+      const parsedData = JSON.parse(storedData || '[]');
 
-        if (parsedData.length > 0) {
-          const fetchedPosts = await Promise.all(
-            parsedData.map(async (post: {post_id: number}) => {
-              const response = await getPost(post.post_id);
-              return response.data.data; // 각 post의 데이터
-            }),
-          );
+      const fetchedPosts = await Promise.all(
+        parsedData.map(async (post: {post_id: number}) => {
+          const response = await getPost(post.post_id);
+          // console.log('getPost 호출 결과값: ', response.data.data);
+          return response.data.data;
+        }),
+      );
 
-          setSavedPosts(fetchedPosts);
-          setCount(fetchedPosts.length);
-        }
-      } catch (error) {
+      setSavedPosts(fetchedPosts);
+      setCount(fetchedPosts.length);
+      // console.log('Fetched Posts:', fetchedPosts);
+
+      if (parsedData.length === 0) {
+        setCount(0);
+        setSavedPosts([]);
+        return;
+      }
+    } catch (error) {
+      if (count === 0) {
+        setSavedPosts([]);
+      } else {
         console.error('Error fetching saved posts:', error);
         Alert.alert(
           '임시 저장 목록을 불러오는 도중 문제가 발생했습니다. 다시 시도해주세요.',
         );
       }
-    };
+    } finally {
+      setIsLoading(false);
+    }
+  };
 
+  // 삭제 시 UI에 반영하기 위한 함수
+  useEffect(() => {
     fetchSavedPosts();
   }, []);
 
@@ -65,10 +79,19 @@ const SavePage: React.FC = () => {
     setSelectedSubtitle(`삭제된 글은 복구할 수 없습니다`);
     setTopButton('삭제하기');
     setBottomButton('취소하기');
-    setSelectedPostId(postId); // 선택된 post_id 설정
+    setSelectedPostId(postId);
   };
 
   // console.log('storageData: ', savedPosts);
+  // console.log('저장된 임시 저장글 개수: ', count);
+
+  if (isLoading) {
+    return (
+      <>
+        <ActivityIndicator size="large" />
+      </>
+    );
+  }
 
   return (
     <>
@@ -78,39 +101,46 @@ const SavePage: React.FC = () => {
             <View style={SaveStyles.count_container}>
               <Text style={SaveStyles.count_text}>총 {count}개</Text>
             </View>
-
-            {savedPosts.map(item => (
-              <View key={item.id} style={SaveStyles.list_container}>
-                <View style={SaveStyles.list}>
-                  <View style={SaveStyles.sub_container}>
-                    <Text style={SaveStyles.list_title}>{item.title}</Text>
-                    <TouchableOpacity
-                      onPress={() =>
-                        openModal(
-                          selectedTitle,
-                          selectedSubTitle,
-                          topButton,
-                          bottomButton,
-                          item.post_id,
-                        )
-                      }>
-                      <View>
-                        <Text style={SaveStyles.delete}>삭제</Text>
-                      </View>
-                    </TouchableOpacity>
-                  </View>
-                  <View style={SaveStyles.sub_container}>
-                    <Text style={SaveStyles.list_date}>
-                      {timeAgo(item.modified_at)}
-                    </Text>
-                    <Text style={SaveStyles.list_expire_date}>
-                      {item.expireDate}일 뒤 자동 삭제
-                    </Text>
-                  </View>
-                </View>
-                <View style={SaveStyles.line} />
+            {count === 0 ? (
+              <View style={SaveStyles.no_container}>
+                <Text style={SaveStyles.no_text}>
+                  임시 저장된 글이 없습니다.
+                </Text>
               </View>
-            ))}
+            ) : (
+              savedPosts.map(item => (
+                <View key={item.post_id} style={SaveStyles.list_container}>
+                  <View style={SaveStyles.list}>
+                    <View style={SaveStyles.sub_container}>
+                      <Text style={SaveStyles.list_title}>{item.title}</Text>
+                      <TouchableOpacity
+                        onPress={() =>
+                          openModal(
+                            selectedTitle,
+                            selectedSubTitle,
+                            topButton,
+                            bottomButton,
+                            item.post_id,
+                          )
+                        }>
+                        <View>
+                          <Text style={SaveStyles.delete}>삭제</Text>
+                        </View>
+                      </TouchableOpacity>
+                    </View>
+                    <View style={SaveStyles.sub_container}>
+                      <Text style={SaveStyles.list_date}>
+                        {timeAgo(item.modified_at)}
+                      </Text>
+                      <Text style={SaveStyles.list_expire_date}>
+                        {item.expireDate}일 뒤 자동 삭제
+                      </Text>
+                    </View>
+                  </View>
+                  <View style={SaveStyles.line} />
+                </View>
+              ))
+            )}
           </View>
         </ScrollView>
       </SafeAreaView>
@@ -120,7 +150,7 @@ const SavePage: React.FC = () => {
       </Text>
 
       {/* 모달 컴포넌트 */}
-      <AlertModal
+      <DeleteTempModal
         modalVisible={modalVisible}
         setModalVisible={setModalVisible}
         title={selectedTitle}
@@ -128,6 +158,7 @@ const SavePage: React.FC = () => {
         topButton={topButton}
         bottomButton={bottomButton}
         postId={selectedPostId}
+        fetchSavedPosts={fetchSavedPosts}
       />
     </>
   );
