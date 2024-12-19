574517c9463b6e59baefe422cfbf619468080afd
Hyewon Yoon <iey704@gachon.ac.kr>
2024-12-19 17:56:37 +0900

    feat: 게시판 검색 결과 화면 구현

---
+++ src/pages/dashboard/search/SearchList.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import ItemPostNoCategory from '@/components/list/ItemPostNoCategory';
+
+type SearchListProps = {
+  postData: any[];
+  selectedFilter: '최신순' | '인기순';
+};
+
+const SearchList: React.FC<SearchListProps> = ({postData, selectedFilter}) => {
+  const sortedPostData =
+    selectedFilter === '최신순'
+      ? [...postData].sort(
+          (a, b) =>
+            new Date(b.created_at).getTime() - new Date(a.created_at).getTime(),
+        )
+      : [...postData].sort((a, b) => b.like_count - a.like_count);
+
+  return <ItemPostNoCategory postList={sortedPostData} />;
+};
+
+export default SearchList;
