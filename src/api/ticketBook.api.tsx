224e6a71d1b2911fc45cf7138d88c5c126119167
Hyewon Yoon <iey704@gachon.ac.kr>
2025-01-07 00:41:16 +0900

    fix: 충돌 해결

--- src/api/ticketBook.api.tsx
+++ src/api/ticketBook.api.tsx
@@ -1,5 +1,5 @@
 import httpApi from './http.api';
 
-export const getTicketList = (dateRange: string) => {
-  return httpApi.get(`/api/v1/ticket/list`, {params: {dateRange}});
+export const getTicketList = (dataRange: string) => {
+  return httpApi.get(`/api/v1/ticket/list`, {params: {dataRange}});
 };
