e807ac9beb84f4bfbf3d5aad48509a0c0504aff9
Hyewon Yoon <iey704@gachon.ac.kr>
2025-01-07 23:16:21 +0900

    feat: 프리미엄 리뷰 조회 API 연결

--- src/pages/home/musical/MusicalDetailReview.tsx
+++ src/pages/home/musical/MusicalDetailReview.tsx
@@ -1,12 +1,40 @@
-import React from 'react';
-import {View, Text, Image} from 'react-native';
+import React, {useCallback, useState} from 'react';
+import {View, Text, Image, Alert, FlatList} from 'react-native';
 import MusicalDetailStyles from './MusicalDetailStyles';
+import {getMusicalReviews} from '@/api/musical.api';
+import {useFocusEffect} from '@react-navigation/native';
+import ItemReview from '@/components/premium/ItemReview';
 
-const MusicalDetailReview: React.FC = () => {
+type MMusicalDetailReviewProps = {
+  data: any;
+};
+
+const MusicalDetailReview: React.FC<MMusicalDetailReviewProps> = ({data}) => {
+  const [reviewInfo, setReviewInfo] = useState();
+
+  const fetchMusicalReviews = async () => {
+    try {
+      const response = await getMusicalReviews(data.id);
+      console.log('프리미엄 리뷰 조회: ', response.data.data);
+      setReviewInfo(response.data.data);
+    } catch (error) {
+      console.log(error);
+      Alert.alert('프리미엄 리뷰 조회 중 문제가 발생했습니다.');
+    }
+  };
+
+  useFocusEffect(
+    useCallback(() => {
+      fetchMusicalReviews();
+    }, []),
+  );
+
   return (
     <View style={MusicalDetailStyles.infoContainer}>
       <View>
-        <Text style={MusicalDetailStyles.infoTitle}>앙코르 평점 4.3</Text>
+        <Text style={MusicalDetailStyles.infoTitle}>
+          앙코르 평점 {data.average_total_rating}
+        </Text>
         <View style={MusicalDetailStyles.reviewsContainer}>
           <View style={MusicalDetailStyles.reviews}>
             <View style={MusicalDetailStyles.review_container}>
@@ -56,6 +84,13 @@ const MusicalDetailReview: React.FC = () => {
         <Text style={MusicalDetailStyles.infoTitle}>프리미엄 리뷰</Text>
         <Text style={MusicalDetailStyles.textWriteReview}>전체보기 {'>'}</Text>
       </View>
+
+      <FlatList
+        data={data.reviews}
+        keyExtractor={item => item.id.toString()}
+        renderItem={({item}) => <ItemReview postList={[item]} />}
+        nestedScrollEnabled={true}
+      />
     </View>
   );
 };
