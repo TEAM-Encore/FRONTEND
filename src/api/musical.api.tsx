b8d113dc6055813256594b7468588802d530b08d
Hyewon Yoon <iey704@gachon.ac.kr>
2025-01-07 23:14:44 +0900

    feat: 인기 뮤지컬, 개봉 예정 뮤지컬 정보 조회 API 연결

---
+++ src/api/musical.api.tsx
@@ -0,0 +1,17 @@
+import httpApi from './http.api';
+
+export const getDetailedMusical = (musical_id: number) => {
+  return httpApi.get(`/api/v1/musical/${musical_id}`);
+};
+
+export const getFeturedMusical = () => {
+  return httpApi.get(`/api/v1/musical/featured`);
+};
+
+export const getUpcomingMusical = () => {
+  return httpApi.get(`/api/v1/musical/upcoming`);
+};
+
+export const getMusicalReviews = (musical_id: number) => {
+  return httpApi.get(`/api/v1/review/musical/${musical_id}/reviews`);
+};
