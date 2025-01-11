5f211d03ebc536d5facc34b6e79bc82a0fcef8bd
Hyewon Yoon <iey704@gachon.ac.kr>
2025-01-11 23:56:49 +0900

    feat: 프리미엄 자신 후기 조회 api 연결

--- src/api/review.api.tsx
+++ src/api/review.api.tsx
@@ -3,3 +3,7 @@ import httpApi from './http.api';
 export const postTicketReview = (ticket_id: number, requestData: any) => {
   return httpApi.post(`/api/v1/review/${ticket_id}`, requestData);
 };
+
+export const getTicketReview = (review_id: number) => {
+  return httpApi.get(`/api/v1/review/${review_id}`);
+};
