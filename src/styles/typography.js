7a50251cbeab6e506f1a9514cb070c2019eae383
Hyewon Yoon <iey704@gachon.ac.kr>
2025-01-17 23:01:44 +0900

    fix: 폰트 오류 해결

--- src/styles/typography.js
+++ src/styles/typography.js
@@ -1,77 +1,77 @@
 export const typography = {
   // Title
   display05: {
-    fontFamily: 'Pretendard-Bold',
+    fontFamily: 'Pretendard Bold',
     fontSize: 40,
     lineHeight: 52,
     letterSpacing: -0.3,
   },
   display04: {
-    fontFamily: 'Pretendard-Bold',
+    fontFamily: 'Pretendard Bold',
     fontSize: 36,
     lineHeight: 46,
     letterSpacing: -0.3,
   },
   display03: {
-    fontFamily: 'Pretendard-Bold',
+    fontFamily: 'Pretendard Bold',
     fontSize: 32,
     lineHeight: 42,
     letterSpacing: -0.3,
   },
   display02: {
-    fontFamily: 'Pretendard-Bold',
+    fontFamily: 'Pretendard Bold',
     fontSize: 28,
     lineHeight: 38,
     letterSpacing: -0.3,
   },
   display01: {
-    fontFamily: 'Pretendard-Bold',
+    fontFamily: 'Pretendard Bold',
     fontSize: 24,
     lineHeight: 34,
     letterSpacing: -0.3,
   },
   headline: {
-    fontFamily: 'Pretendard-Bold',
+    fontFamily: 'Pretendard Bold',
     fontSize: 20,
     lineHeight: 28,
     letterSpacing: -0.3,
   },
   subhead05: {
-    fontFamily: 'Pretendard-SemiBold',
+    fontFamily: 'Pretendard SemiBold',
     fontSize: 20,
     lineHeight: 28,
     letterSpacing: -0.3,
   },
   subhead04: {
-    fontFamily: 'Pretendard-SemiBold',
+    fontFamily: 'Pretendard SemiBold',
     fontSize: 18,
   },
   subhead03: {
-    fontFamily: 'Pretendard-SemiBold',
+    fontFamily: 'Pretendard SemiBold',
     fontSize: 16,
     lineHeight: 22,
     letterSpacing: -0.3,
   },
   subheadLong03: {
-    fontFamily: 'Pretendard-Medium',
+    fontFamily: 'Pretendard Medium',
     fontSize: 16,
     lineHeight: 28,
     letterSpacing: -0.3,
   },
   subhead02: {
-    fontFamily: 'Pretendard-SemiBold',
+    fontFamily: 'Pretendard SemiBold',
     fontSize: 14,
     lineHeight: 20,
     letterSpacing: -0.3,
   },
   subheadLong02: {
-    fontFamily: 'Pretendard-Medium',
+    fontFamily: 'Pretendard Medium',
     fontSize: 14,
     lineHeight: 24,
     letterSpacing: -0.3,
   },
   subhead01: {
-    fontFamily: 'Pretendard-SemiBold',
+    fontFamily: 'Pretendard SemiBold',
     fontSize: 12,
     lineHeight: 18,
     letterSpacing: -0.3,
@@ -79,19 +79,19 @@ export const typography = {
 
   // Body
   body02: {
-    fontFamily: 'Pretendard-Regular',
+    fontFamily: 'Pretendard Regular',
     fontSize: 16,
     lineHeight: 24,
     letterSpacing: -0.3,
   },
   bodyLong02: {
-    fontFamily: 'Pretendard-Regular',
+    fontFamily: 'Pretendard Regular',
     fontSize: 16,
     lineHeight: 28,
     letterSpacing: -0.3,
   },
   body01: {
-    fontFamily: 'Pretendard-Regular',
+    fontFamily: 'Pretendard Regular',
     fontSize: 14,
     lineHeight: 20,
     letterSpacing: -0.3,
