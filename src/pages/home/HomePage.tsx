22f0087589304eaa30aadefe74009f6cbf5fce52
Hyewon Yoon <iey704@gachon.ac.kr>
2025-01-18 03:58:12 +0900

    fix: 홈페이지 경고 표시 해결

--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -624,6 +624,9 @@ const HomePage: React.FC<HomePageProps> = () => {
           <Text style={HomeStyles.textTitle}>개봉 예정 뮤지컬</Text>
         </View>
         <FlatList
+          keyExtractor={(item, index) =>
+            item.id?.toString() || index.toString()
+          }
           style={{marginHorizontal: 12.5, marginBottom: 37}}
           ref={flatListRef}
           data={releaseMusicalInfo}
