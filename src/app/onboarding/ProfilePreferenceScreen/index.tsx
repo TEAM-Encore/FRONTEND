3fd5af9597ac62303d920ff4d2d449d432d7690e
Eunzo <wannabeeung@gmail.com>
2025-07-19 00:06:56 +0900

    ✨ Add ProfilePreferenceScreen

--- src/app/onboarding/ProfilePreferenceScreen/index.tsx
+++ src/app/onboarding/ProfilePreferenceScreen/index.tsx
@@ -1,14 +1,12 @@
-import {View, Text} from 'react-native';
-import React from 'react';
+import React, {useState} from 'react';
 import {SafeAreaView} from 'react-native-safe-area-context';
-import styled from 'styled-components/native';
-import Header from '@/components/Header';
 import OnboardingHeader from '@/features/onboarding/modules/OnboardingHeader';
-import Typo from '@/components/Typo';
 import OnboardingNextButton from '@/features/onboarding/modules/OnboardingNextButton';
 import ProfilePreferenceGrid from '@/features/onboarding/modules/ProfilePreference/ProfilePreferenceGrid';
 
 export default function ProfilePreferenceScreen() {
+  const [selectedKeywords, setSelectedKeywords] = useState<string[]>([]);
+
   return (
     <SafeAreaView style={{flex: 1}}>
       <OnboardingHeader
@@ -16,8 +14,11 @@ export default function ProfilePreferenceScreen() {
         description="선호하는 공연 키워드를 선택해주세요."
         step={2}
       />
-      <ProfilePreferenceGrid />
-      <OnboardingNextButton disabled={true} />
+      <ProfilePreferenceGrid
+        setSelectedKeywords={setSelectedKeywords}
+        selectedKeywords={selectedKeywords}
+      />
+      <OnboardingNextButton disabled={selectedKeywords.length === 0} />
     </SafeAreaView>
   );
 }
--- src/features/onboarding/modules/ProfilePreference/ProfilePreferenceGrid.tsx
+++ src/features/onboarding/modules/ProfilePreference/ProfilePreferenceGrid.tsx
@@ -3,23 +3,66 @@ import React from 'react';
 import Typo from '@/components/Typo';
 import styled, {useTheme} from 'styled-components/native';
 
-const images = [
-  require('@/assets/images/onboarding/acting.png'),
-  require('@/assets/images/onboarding/actor.png'),
-  require('@/assets/images/onboarding/casting.png'),
-  require('@/assets/images/onboarding/emotional.png'),
-  require('@/assets/images/onboarding/entertaining.png'),
-  require('@/assets/images/onboarding/light.png'),
-  require('@/assets/images/onboarding/quality.png'),
-  require('@/assets/images/onboarding/stage-design.png'),
-  require('@/assets/images/onboarding/story.png'),
-];
-
 const {width} = Dimensions.get('window');
 const itemWidth = (width - 40 - 32) / 3;
 
-export default function ProfilePreferenceGrid() {
+export interface Props {
+  setSelectedKeywords: (keywords: string[]) => void;
+  selectedKeywords: string[];
+}
+
+export default function ProfilePreferenceGrid({
+  setSelectedKeywords,
+  selectedKeywords,
+}: Props) {
   const theme = useTheme();
+  const gridItems = [
+    {
+      id: 'EMOTIONAL',
+      image: require('@/assets/images/onboarding/emotional.png'),
+      selectedImage: require('@/assets/images/onboarding/emotional-selected.png'),
+    },
+    {
+      id: 'ENTERTAINING',
+      image: require('@/assets/images/onboarding/entertaining.png'),
+      selectedImage: require('@/assets/images/onboarding/entertaining-selected.png'),
+    },
+    {
+      id: 'QUALITY',
+      image: require('@/assets/images/onboarding/quality.png'),
+      selectedImage: require('@/assets/images/onboarding/quality-selected.png'),
+    },
+    {
+      id: 'STAGE_DESIGN',
+      image: require('@/assets/images/onboarding/stage-design.png'),
+      selectedImage: require('@/assets/images/onboarding/stage-design-selected.png'),
+    },
+    {
+      id: 'CASTING',
+      image: require('@/assets/images/onboarding/casting.png'),
+      selectedImage: require('@/assets/images/onboarding/casting-selected.png'),
+    },
+    {
+      id: 'ACTING',
+      image: require('@/assets/images/onboarding/acting.png'),
+      selectedImage: require('@/assets/images/onboarding/acting-selected.png'),
+    },
+    {
+      id: 'ACTOR',
+      image: require('@/assets/images/onboarding/actor.png'),
+      selectedImage: require('@/assets/images/onboarding/actor-selected.png'),
+    },
+    {
+      id: 'LIGHT',
+      image: require('@/assets/images/onboarding/light.png'),
+      selectedImage: require('@/assets/images/onboarding/light-selected.png'),
+    },
+    {
+      id: 'STORY',
+      image: require('@/assets/images/onboarding/story.png'),
+      selectedImage: require('@/assets/images/onboarding/story-selected.png'),
+    },
+  ];
 
   return (
     <View style={{paddingHorizontal: 20, marginTop: 30}}>
@@ -28,17 +71,33 @@ export default function ProfilePreferenceGrid() {
       </Typo.Body01>
 
       <Container>
-        {images.map((image, index) => (
-          <GridItem key={index}>
-            <Image
-              source={image}
-              resizeMode="contain"
-              style={{
-                width: '100%',
-                height: '100%',
-              }}
-              tintColor={'indigo'}
-            />
+        {gridItems.map((item, index) => (
+          <GridItem
+            key={index}
+            onPress={() => {
+              if (selectedKeywords.includes(item.id)) {
+                setSelectedKeywords(
+                  selectedKeywords.filter(keyword => keyword !== item.id),
+                );
+              } else {
+                if (selectedKeywords.length < 3) {
+                  setSelectedKeywords([...selectedKeywords, item.id]);
+                }
+              }
+            }}>
+            {selectedKeywords.includes(item.id) ? (
+              <Image
+                source={item.selectedImage}
+                resizeMode="contain"
+                style={{width: '100%', height: '100%'}}
+              />
+            ) : (
+              <Image
+                source={item.image}
+                resizeMode="contain"
+                style={{width: '100%', height: '100%'}}
+              />
+            )}
           </GridItem>
         ))}
       </Container>
@@ -53,7 +112,7 @@ const Container = styled.View`
   margin-top: 16px;
 `;
 
-const GridItem = styled.View`
+const GridItem = styled.TouchableOpacity`
   width: ${itemWidth}px;
   aspect-ratio: 1;
   border-radius: 8px;
